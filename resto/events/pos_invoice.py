6cfd8fdd61d103cda30e8b175bb7b617093f4e68
auliarch <auliarchmn22@gmail.com>
2025-11-20 16:35:04 +0700

    update override pos_invoice

--- resto/events/pos_invoice.py
+++ resto/events/pos_invoice.py
@@ -11,16 +11,20 @@ def exclude_void_items_from_total(doc, method):
             net_total += (item.net_amount or 0)
             base_net_total += (item.base_net_amount or 0)
 
+    # total sebelum diskon
     doc.total = total
     doc.base_total = total
+
+    # total setelah diskon
     doc.net_total = net_total
     doc.base_net_total = base_net_total
 
     taxes = doc.total_taxes_and_charges or 0
     base_taxes = doc.base_total_taxes_and_charges or 0
 
-    doc.grand_total = total + taxes
-    doc.base_grand_total = total + base_taxes
+    # ✔ grand total HARUS pakai net_total
+    doc.grand_total = net_total + taxes
+    doc.base_grand_total = base_net_total + base_taxes
 
     doc.rounded_total = frappe.utils.flt(doc.grand_total, doc.precision("rounded_total"))
     doc.base_rounded_total = frappe.utils.flt(doc.base_grand_total, doc.precision("base_rounded_total"))
