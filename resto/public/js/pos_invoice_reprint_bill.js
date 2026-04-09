bc51ce92cbcac3cdcce2e9b09f3424e57e87e39e
auliarch <auliarchmn22@gmail.com>
2026-04-09 09:36:45 +0700

    add confirmation reprint bill

--- resto/public/js/pos_invoice_reprint_bill.js
+++ resto/public/js/pos_invoice_reprint_bill.js
@@ -3,22 +3,28 @@ frappe.ui.form.on("POS Invoice", {
 
         if (!frm.is_new() && frm.doc.docstatus === 1) {
             frm.add_custom_button("Print Bill", () => {
-
-                frappe.call({
-                    method: "resto.api.print_receipt_now",
-                    args: {
-                        invoice_name: frm.doc.name,
-                        branch: frm.doc.branch
+                frappe.confirm(
+                    "Yakin ingin print ulang bill ini?",                    
+                    () => {
+                        frappe.call({
+                            method: "resto.api.print_receipt_now",
+                            args: {
+                                invoice_name: frm.doc.name,
+                                branch: frm.doc.branch
+                            },
+                            freeze: true,
+                            freeze_message: "Printing...",
+                            callback: function(r) {
+                                if (!r.exc) {
+                                    frappe.msgprint("Print berhasil");
+                                }
+                            }
+                        });
                     },
-                    freeze: true,
-                    freeze_message: "Printing...",
-                    callback: function(r) {
-                        if (!r.exc) {
-                            frappe.msgprint("Print berhasil");
-                        }
+                    () => {
+                        frappe.msgprint("Print dibatalkan");
                     }
-                });
-
+                );
             });
         }
     }
